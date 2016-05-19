2e8be5b423c74045f0cf9a52d477db405654593b
Ari Crellin-Quick <a.crellinquick@gmail.com>
2016-05-18 21:57:51 -0700

    Add datasets & featurize tabs (incomplete)

--- flask_server.py
+++ flask_server.py
@@ -9,10 +9,21 @@ from flask import (
     Flask, request, abort, render_template,
     session, Response, jsonify, g, send_from_directory)
 
+from config import cfg
+from cesium import obs_feature_tools as oft
+from cesium import science_feature_tools as sft
+from cesium import data_management
+from cesium import time_series as tslib
+from cesium import transformation
+from cesium import featurize
+from cesium import predict
+from cesium import build_model
+
 
 # Flask initialization
 app = Flask(__name__, static_url_path='', static_folder='public')
-app.add_url_rule('/', 'root', lambda: app.send_static_file('mock_projects_page_react.html'))
+app.add_url_rule('/', 'root',
+                 lambda: app.send_static_file('mock_projects_page_react.html'))
 
 # RethinkDB config:
 RDB_HOST = os.environ.get('RDB_HOST') or 'localhost'
@@ -26,7 +37,8 @@ if not ('--help' in sys.argv or '--install' in sys.argv):
         rdb_conn = rdb.connect(host=RDB_HOST, port=RDB_PORT, db=CESIUM_DB)
     except rdb.errors.RqlDriverError as e:
         print(e)
-        print('Unable to connect to RethinkDB.  Please ensure that it is running.')
+        print('Unable to connect to RethinkDB. '
+              'Please ensure that it is running.')
         sys.exit(-1)
 
 
@@ -123,7 +135,11 @@ def get_list_of_projects():
 
     """
     if request.method == 'GET':
-        return Response(json.dumps(list_projects(auth_only=False, name_only=False)), mimetype='application/json', headers={'Cache-Control': 'no-cache', 'Access-Control-Allow-Origin': '*'})
+        return Response(json.dumps(list_projects(auth_only=False,
+                                                 name_only=False)),
+                        mimetype='application/json',
+                        headers={'Cache-Control': 'no-cache',
+                                 'Access-Control-Allow-Origin': '*'})
 
 
 @app.route('/get_state', methods=['GET'])
@@ -270,7 +286,6 @@ def list_projects(auth_only=True, name_only=False):
     return [entry for entry in rdb.table('projects').run(g.rdb_conn)]
 
 
-
 def add_project(name, desc="", addl_authed_users=[], user_email="auto"):
     """Add a new entry to the rethinkDB 'projects' table.
 
@@ -304,6 +319,36 @@ def add_project(name, desc="", addl_authed_users=[], user_email="auto"):
         "created": str(rdb.now().in_timezone('-08:00').run(g.rdb_conn))
     }).run(g.rdb_conn)['generated_keys'][0]
     return new_projkey
+
+
+def add_dataset(name, projkey):
+    """Add a new entry to the rethinkDB 'datasets' table.
+
+    Parameters
+    ----------
+    name : str
+        New dataset name.
+    projkey : str
+        RethinkDB key/ID of parent project.
+
+    Returns
+    -------
+    str
+        RethinkDB key/ID of newly created dataset entry.
+
+    """
+    new_dataset_id = rdb.table('datasets').insert({
+        'projkey': projkey,
+        'name': name,
+        'created': str(rdb.now().in_timezone('-08:00').run(g.rdb_conn)),
+    }).run(g.rdb_conn)['generated_keys'][0]
+    print("Dataset %s entry added to cesium_app db." % name)
+    return new_dataset_id
+
+
+def set_dataset_filenames(dataset_id, ts_filenames):
+    rdb.table('datasets').get(dataset_id).update({'ts_filenames':
+                                                  ts_filenames}).run(rdb_conn)
 
 
 def delete_project_by_key(project_key):
@@ -355,7 +400,8 @@ def newProject():
 
         proj_description = str(request.form["Description/notes"]).strip()
 
-        addl_users = str(request.form["Additional Authorized Users"]).strip().split(',')
+        addl_users = str(request.form["Additional Authorized Users"])\
+            .strip().split(',')
         if addl_users in [[''], ["None"]]:
             addl_users = []
         if "user_email" in request.form:
@@ -372,7 +418,11 @@ def newProject():
         new_projkey = add_project(
             proj_name, desc=proj_description, addl_authed_users=addl_users,
             user_email=user_email)
-        return Response(json.dumps(list_projects(auth_only=False, name_only=False)), mimetype='application/json', headers={'Cache-Control': 'no-cache', 'Access-Control-Allow-Origin': '*'})
+        return Response(json.dumps(list_projects(auth_only=False,
+                                                 name_only=False)),
+                        mimetype='application/json',
+                        headers={'Cache-Control': 'no-cache',
+                                 'Access-Control-Allow-Origin': '*'})
 
 
 @app.route('/deleteProject', methods=['POST'])
@@ -389,8 +439,144 @@ def deleteProject():
         proj_key = str(request.form["project_key"])
 
         result = delete_project_by_key(proj_key)
-        return Response(json.dumps(list_projects(auth_only=False, name_only=False)), mimetype='application/json', headers={'Cache-Control': 'no-cache', 'Access-Control-Allow-Origin': '*'})
+        return Response(json.dumps(list_projects(auth_only=False,
+                                                 name_only=False)),
+                        mimetype='application/json',
+                        headers={'Cache-Control': 'no-cache',
+                                 'Access-Control-Allow-Origin': '*'})
+
+
+@app.route(('/uploadData/<dataset_name>/<headerfile>/<zipfile>/<project_name>'),
+           methods=['POST'])
+@app.route('/uploadData', methods=['POST'])
+def uploadData(dataset_name=None, headerfile=None, zipfile=None, project_name=None):
+    """Save uploaded time series data files
+
+    Handles POST form submission.
+
+    Returns
+    -------
+    Redirects to featurizationPage, see that function for output
+    details.
+
+    """
+    # TODO: ADD MORE ROBUST EXCEPTION HANDLING (HERE AND ALL OTHER FUNCTIONS)
+    if request.method == 'POST':
+        post_method = "browser"
+        # Parse form fields
+        dataset_name = str(request.form["Dataset Name"]).strip()
+        headerfile = request.files["Header File"]
+        zipfile = request.files["Tarball Containing Data"]
+        if dataset_name == "":
+            return jsonify({
+                "message": ("Dataset Title must contain non-whitespace "
+                            "characters. Please try a different title."),
+                "type": "error"})
+        projkey = (request.form["Select Project"])
+        # Create unique file names
+        headerfile_name = (str(uuid.uuid4()) + "_" +
+                           str(secure_filename(headerfile.filename)))
+        zipfile_name = (str(uuid.uuid4()) + "_" +
+                        str(secure_filename(zipfile.filename)))
+        headerfile_path = pjoin(cfg['paths']['upload_folder'], headerfile_name)
+        zipfile_path = pjoin(cfg['paths']['upload_folder'], zipfile_name)
+        headerfile.save(headerfile_path)
+        zipfile.save(zipfile_path)
+        print("Saved", headerfile_name, "and", zipfile_name)
+        try:
+            check_headerfile_and_tsdata_format(headerfile_path, zipfile_path)
+        except custom_exceptions.DataFormatError as err:
+            os.remove(headerfile_path)
+            os.remove(zipfile_path)
+            print("Removed", headerfile_name, "and", zipfile_name)
+            return jsonify({"message": str(err), "type": "error"})
+        except custom_exceptions.TimeSeriesFileNameError as err:
+            os.remove(headerfile_path)
+            os.remove(zipfile_path)
+            print("Removed", headerfile_name, "and", zipfile_name)
+            return jsonify({"message": str(err), "type": "error"})
+        except:
+            raise
+        new_dataset_id = add_dataset(name=dataset_name, projkey=projkey)
+        time_series = data_management.parse_and_store_ts_data(
+            zipfile_path, cfg['paths']['ts_data_folder'], headerfile_path,
+            new_dataset_id)
+        ts_paths = [ts.path for ts in time_series]
+        set_dataset_filenames(new_dataset_id, ts_paths)
+        return jsonify({
+            "message": "New time series files saved successfully.",
+            "dataset_name": dataset_name,
+            "headerfile_name": headerfile_name, "zipfile_name": zipfile_name,
+            "dataset_id": new_dataset_id,
+            "datasetsList": list_datasets(auth_only=False)})
+
+
+@app.route(('/FeaturizeData/<dataset_id>/<project_name>'
+            '/<featureset_name>/<features_to_use>/<custom_features_script>/'
+            '<user_email>/<email_user>/<is_test>'), methods=['POST'])
+@app.route('/FeaturizeData', methods=['POST', 'GET'])
+def FeaturizeData(
+        dataset_id=None, project_name=None,
+        featureset_name=None, features_to_use=None,
+        custom_features_script=None, user_email=None, email_user=False,
+        is_test=False):
+    """Save uploaded time series data files and begin featurization.
+
+    Handles POST form submission.
+
+    Returns
+    -------
+    Redirects to featurizationPage, see that function for output
+    details.
+
+    """
+    # TODO: ADD MORE ROBUST EXCEPTION HANDLING (HERE AND ALL OTHER FUNCTIONS)
+    if request.method == 'POST':
+        post_method = "browser"
+        # Parse form fields
+        featureset_name = str(request.form["featureset_name"]).strip()
+        if featureset_name == "":
+            return jsonify({
+                "message": ("Feature Set Title must contain non-whitespace "
+                            "characters. Please try a different title."),
+                "type": "error"})
+        dataset_id = str(request.form["featureset_dataset_select"]).strip()
+        project_name = (str(request.form["featureset_project_name_select"]).
+                        strip().split(" (created")[0])
+        features_to_use = request.form.getlist("features_selected")
+        custom_script_tested = str(request.form["custom_script_tested"])
+        if custom_script_tested == "yes":
+            custom_script = request.files["custom_feat_script_file"]
+            customscript_fname = str(secure_filename(custom_script.filename))
+            print(customscript_fname, 'uploaded.')
+            customscript_path = pjoin(
+                    cfg['paths']['upload_folder'], "custom_feature_scripts",
+                    str(uuid.uuid4()) + "_" + str(customscript_fname))
+            custom_script.save(customscript_path)
+            custom_features = request.form.getlist("custom_feature_checkbox")
+            features_to_use += custom_features
+        else:
+            customscript_path = False
+        print("Selected features:", features_to_use)
+        try:
+            email_user = request.form["email_user"]
+            if email_user == "True":
+                email_user = True
+        except:  # unchecked
+            email_user = False
+        try:
+            is_test = request.form["is_test"]
+            if is_test == "True":
+                is_test = True
+        except:  # unchecked
+            is_test = False
+        proj_key = project_name_to_key(project_name)
+        return featurizationPage(
+            featureset_name=featureset_name, project_name=project_name,
+            dataset_id=dataset_id,
+            featlist=features_to_use, is_test=is_test,
+            email_user=email_user, custom_script_path=customscript_path)
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=4000)# port=int(os.environ.get("PORT", 4000)))
+    app.run(debug=True, port=4000)
