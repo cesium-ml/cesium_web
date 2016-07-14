558c4285eb669b56062124c0a93805a34ab0310f
Stefan van der Walt <stefanv@berkeley.edu>
2016-07-14 09:29:26 -0700

    postgres_init: do not abort on error

--- docker/postgres/init-user-db.sh
+++ docker/postgres/init-user-db.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+psql -v ON_ERROR_STOP=0 --username "$POSTGRES_USER" <<-EOSQL
   CREATE USER cesium;
   CREATE DATABASE cesium;
   GRANT ALL PRIVILEGES ON DATABASE cesium TO cesium;
