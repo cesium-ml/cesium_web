a3b59fe1e2b329944333b75e3631204afc898e44
Stefan van der Walt <stefanv@berkeley.edu>
2016-07-14 02:57:23 -0700

    Cannot load cesium from db init script
    (That data is on another container)

--- docker/postgres/init-user-db.sh
+++ docker/postgres/init-user-db.sh
@@ -7,7 +7,3 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
   GRANT ALL PRIVILEGES ON DATABASE cesium TO cesium;
 EOSQL
 
-cd /cesium
-source /cesium_env/bin/activate && \
-  PYTHONPATH=. python -c "from cesium_app.models import create_tables as c; c()"
-
