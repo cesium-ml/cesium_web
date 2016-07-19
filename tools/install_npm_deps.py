f0a572597d1e78585d648974d8f961d29a75984f
Brett Naul <brettnaul@gmail.com>
2016-07-19 11:09:11 -0700

    More backwards-compatible argument unpacking

--- tools/install_npm_deps.py
+++ tools/install_npm_deps.py
@@ -15,7 +15,8 @@ pkg_file = sys.argv[1]
 
 with open(pkg_file) as f:
     deps = json.load(f)
-    all_deps = {**deps['dependencies'], **deps['devDependencies']}
+    all_deps = deps['dependencies']
+    all_deps.update(deps['devDependencies'])
 
 for dep in all_deps:
     pkg_path = pjoin(os.path.dirname(os.path.abspath(pkg_file)))
