787236c8834eeb359dab64ed652d7423332bc157
Ari Crellin-Quick <a.crellinquick@gmail.com>
2016-08-22 14:07:43 -0700

    Add model hyperparameter frontend test

--- cesium_app/tests/test_models.py
+++ cesium_app/tests/test_models.py
@@ -55,3 +55,33 @@ def test_delete_model(driver):
         driver.implicitly_wait(1)
         status_td = driver.find_element_by_xpath(
             "//div[contains(text(),'Model successfully deleted')]")
+
+
+def test_hyper_param_populate(driver):
+    driver.get('http://localhost:5000')
+    driver.set_window_size(1920,1080)
+    with create_test_project() as p, create_test_dataset(p) as ds,\
+         create_test_featureset(p) as fs, create_test_model(fs) as m:
+        driver.refresh()
+        proj_select = Select(driver.find_element_by_css_selector('[name=project]'))
+        proj_select.select_by_value(str(p.id))
+
+        driver.find_element_by_id('react-tabs-6').click()
+
+        driver.find_element_by_partial_link_text('Create New Model').click()
+        driver.find_element_by_partial_link_text('Choose Model Parameters').click()
+        model_select = Select(driver.find_element_by_css_selector('[name=modelType]'))
+        model_select.select_by_visible_text("RandomForestClassifier")
+        driver.implicitly_wait(0.1)
+        driver.find_element_by_xpath("//label[contains(text(),'n_estimators')]")
+        driver.find_element_by_xpath("//label[contains(text(),'max_features')]")
+
+        model_select.select_by_visible_text("RidgeClassifierCV")
+        driver.implicitly_wait(0.1)
+        driver.find_element_by_xpath("//label[contains(text(),'alphas')]")
+        driver.find_element_by_xpath("//label[contains(text(),'scoring')]")
+
+        model_select.select_by_visible_text("BayesianRidgeRegressor")
+        driver.implicitly_wait(0.1)
+        driver.find_element_by_xpath("//label[contains(text(),'n_iter')]")
+        driver.find_element_by_xpath("//label[contains(text(),'alpha_1')]")
