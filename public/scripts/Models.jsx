8ae52d99b7774a0133fc92aaf3d60573a2c3b1ee
Ari Crellin-Quick <a.crellinquick@gmail.com>
2016-11-02 13:23:14 -0700

    Add model info drop-down table

--- public/scripts/Models.jsx
+++ public/scripts/Models.jsx
@@ -10,6 +10,7 @@ import * as Action from './actions';
 import Expand from './Expand';
 import Delete from './Delete';
 import { $try, reformatDatetime } from './utils';
+import FoldableRow from './FoldableRow';
 
 
 const ModelsTab = props => (
@@ -159,29 +160,65 @@ Model.propTypes = {
 };
 
 
+let ModelInfo = props => (
+  <table className="table">
+    <thead>
+      <tr>
+        <th>Model Type</th>
+        <th>Hyper Parameters</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>
+          {props.model.type}
+        </td>
+        <td>
+          {JSON.stringify(props.model.params, null, 4)}
+        </td>
+      </tr>
+    </tbody>
+  </table>
+);
+ModelInfo.propTypes = {
+  model: React.PropTypes.object.isRequired
+};
+
 export let ModelTable = props => (
   <table className="table">
     <thead>
       <tr>
         <th>Name</th><th>Created</th><th>Status</th><th>Actions</th>
       </tr>
+    </thead>
 
-      {
-        props.models.map((model) => {
-          const done = model.finished;
-          const status = done ? <td>Completed {reformatDatetime(model.finished)}</td> : <td>In progress</td>;
+    {
+      props.models.map((model, idx) => {
+        const done = model.finished;
+        const status = done ? <td>Completed {reformatDatetime(model.finished)}</td> : <td>In progress</td>;
 
-          return (
+        const foldedContent = done && (
+          <tr key={`modelinfo_${idx}`}>
+            <td colSpan={6}>
+              <ModelInfo model={model} />
+            </td>
+          </tr>
+        );
+
+        return (
+          <FoldableRow key={`model_${idx}`}>
             <tr key={model.id}>
               <td>{model.name}</td>
               <td>{reformatDatetime(model.created)}</td>
               {status}
               <td><DeleteModel ID={model.id} /></td>
             </tr>
-          ); })
-      }
+            {foldedContent}
+          </FoldableRow>
+        );
+      })
+    }
 
-    </thead>
   </table>
 );
 ModelTable.propTypes = {
