482885a400ee5fbb686f294b8beb59fe3dbc9c76
Stefan van der Walt <stefanv@berkeley.edu>
2017-02-27 11:21:19 -0800

    Remove some unnecessary characters from bash script

--- tools/check_js_updates.sh
+++ tools/check_js_updates.sh
@@ -5,7 +5,7 @@ set -e
 CHECKER=./node_modules/.bin/ncu
 
 if [[ ! -x ${CHECKER} ]]; then
-    npm install npm-check-updates > /dev/null 2>&1; \
+    npm install npm-check-updates > /dev/null 2>&1
 fi
 
 ${CHECKER}
