40d370495e3d9c339efca2c1ffa87592ed906ced
hatsy <hadrien@berkeley.edu>
2017-04-14 15:19:30 -0700

    Updated bokeh plot to reflect original plotly plot

--- cesium_app/plot.py
+++ cesium_app/plot.py
@@ -1,9 +1,9 @@
-from itertools import cycle, islice
+from itertools import cycle
 import numpy as np
 from cesium import featurize
 from bokeh.plotting import figure
 from bokeh.layouts import gridplot
-from bokeh.palettes import PuBu as palette
+from bokeh.palettes import Viridis as palette
 from bokeh.core.json_encoder import serialize_json
 from bokeh.document import Document
 from bokeh.util.serialization import make_id
@@ -22,44 +22,27 @@ def feature_scatterplot(fset_path, features_to_plot):
     Returns
     -------
     (str, str)
-        Returns (script, div) tags for the desired plot as output by
-        `bokeh.embed.components`.
+        Returns (docs_json, render_items) json for the desired plot.
     """
     fset, data = featurize.load_featureset(fset_path)
-    X = fset[features_to_plot]
-    if 'target' in fset and fset.target.values.dtype != np.float:
-        y = fset.target.values
-        labels = np.unique(y)
-    else:
-        y = [None] * len(X)
-        labels = [None]
-
-    if len(labels) in palette:
-        colors = palette[len(labels)]
-    else:
-        all_colors = sorted(palette.items(), key=lambda x: x[0],
-                            reverse=True)[0][1]
-        colors = list(islice(cycle(all_colors), len(labels)))
-
+    fset = fset[features_to_plot]
+    colors = cycle(palette[5])
     plots = np.array([[figure(width=300, height=200)
                        for j in range(len(features_to_plot))]
                       for i in range(len(features_to_plot))])
-    for (i, j), p in np.ndenumerate(plots):
-        for l, c in zip(labels, colors):
-            if l is not None:
-                inds = np.where(y == l)[0]
-            else:
-                inds = np.arange(len(X))
-            p.circle(X.values[inds, i], X.values[inds, j], color=c,
-                     legend=(l if (i == j and l is not None) else None))
-            p.legend.location = 'bottom_right'
-            p.legend.label_text_font_size = '6pt'
-            p.legend.spacing = 0
-            p.legend.padding = 0
-            p.xaxis.axis_label = features_to_plot[i]
-            p.yaxis.axis_label = features_to_plot[j]
 
-    plot = gridplot(plots.tolist(), ncol=len(features_to_plot), mergetools=True)
+    for (j, i), p in np.ndenumerate(plots):
+        if (j == i == 0):
+            p.title.text = "Scatterplot matrix"
+        p.circle(fset.values[:,i], fset.values[:,j], color=next(colors))
+        p.xaxis.minor_tick_line_color = None
+        p.yaxis.minor_tick_line_color = None
+        p.ygrid[0].ticker.desired_num_ticks = 2
+        p.xgrid[0].ticker.desired_num_ticks = 4
+        p.outline_line_color = None
+        p.axis.visible = None
+
+    plot = gridplot(plots.tolist(), ncol=len(features_to_plot), mergetools=True, responsive=True, title="Test")
 
     # Convert plot to json objects necessary for rendering with bokeh on the
     # frontend
