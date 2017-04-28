32fa71178c12db86ef671eeb65e97687999440e0
Stefan van der Walt <stefanv@berkeley.edu>
2017-04-28 10:16:43 -0700

    Add sudo check to db_init for Travis-CI/Docker

--- tools/db_init.sh
+++ tools/db_init.sh
@@ -2,6 +2,13 @@
 
 PLAT=$(uname)
 
+# Travis-CI and Docker doesn't have sudo, so check if it's available
+if [[ -x $(which sudo) ]]; then
+    SUDO="sudo -u postgres"
+else
+    SUDO=""
+fi
+
 if [[ $PLAT == 'Darwin' ]]; then
     echo "Configuring OSX postgres"
     createdb -w cesium
@@ -11,10 +18,10 @@ if [[ $PLAT == 'Darwin' ]]; then
     psql -U cesium -c 'GRANT ALL PRIVILEGES ON DATABASE cesium_test to cesium;'
 else
     echo "Configuring Linux postgres"
-    sudo -u postgres psql -c 'CREATE DATABASE cesium;'
-    sudo -u postgres psql -c 'CREATE DATABASE cesium_test;'
-    sudo -u postgres psql -c 'CREATE USER cesium;'
-    sudo -u postgres psql -c 'GRANT ALL PRIVILEGES ON DATABASE cesium to cesium;'
-    sudo -u postgres psql -c 'GRANT ALL PRIVILEGES ON DATABASE cesium_test to cesium;'
+    $SUDO psql -c 'CREATE DATABASE cesium;'
+    $SUDO psql -c 'CREATE DATABASE cesium_test;'
+    $SUDO psql -c 'CREATE USER cesium;'
+    $SUDO psql -c 'GRANT ALL PRIVILEGES ON DATABASE cesium to cesium;'
+    $SUDO psql -c 'GRANT ALL PRIVILEGES ON DATABASE cesium_test to cesium;'
 fi
 
