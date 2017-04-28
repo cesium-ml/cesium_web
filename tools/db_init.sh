ce6fdf2e8f835e8c48a5cce3a51f232f3f051b89
Stefan van der Walt <stefanv@berkeley.edu>
2017-04-28 10:05:46 -0700

    Improve platform detection for db_init

--- tools/db_init.sh
+++ tools/db_init.sh
@@ -1,19 +1,20 @@
 #!/usr/bin/env bash
 
-_=$(id -u postgres 2>&1)
+PLAT=$(uname)
 
-if [[ $? == 0 ]]; then
-    echo "Configuring Linux postgres"
-    sudo -u postgres psql -c 'CREATE DATABASE cesium;'
-    sudo -u postgres psql -c 'CREATE DATABASE cesium_test;'
-    sudo -u postgres psql -c 'CREATE USER cesium;'
-    sudo -u postgres psql -c 'GRANT ALL PRIVILEGES ON DATABASE cesium to cesium;'
-    sudo -u postgres psql -c 'GRANT ALL PRIVILEGES ON DATABASE cesium_test to cesium;'
-else
+if [[ $PLAT == 'Darwin' ]]; then
     echo "Configuring OSX postgres"
     createdb -w cesium
     createdb -w cesium_test
     createuser cesium
     psql -U cesium -c 'GRANT ALL PRIVILEGES ON DATABASE cesium to cesium;'
     psql -U cesium -c 'GRANT ALL PRIVILEGES ON DATABASE cesium_test to cesium;'
+else
+    echo "Configuring Linux postgres"
+    sudo -u postgres psql -c 'CREATE DATABASE cesium;'
+    sudo -u postgres psql -c 'CREATE DATABASE cesium_test;'
+    sudo -u postgres psql -c 'CREATE USER cesium;'
+    sudo -u postgres psql -c 'GRANT ALL PRIVILEGES ON DATABASE cesium to cesium;'
+    sudo -u postgres psql -c 'GRANT ALL PRIVILEGES ON DATABASE cesium_test to cesium;'
 fi
+
