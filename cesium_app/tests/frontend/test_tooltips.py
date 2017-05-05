2fda1d28883a95eccb258219c0bccc589df6a165
Ari Crellin-Quick <a.crellinquick@gmail.com>
2017-05-05 14:36:23 -0700

    Shorten sleep time during tooltip hover tests

--- cesium_app/tests/frontend/test_tooltips.py
+++ cesium_app/tests/frontend/test_tooltips.py
@@ -12,7 +12,7 @@ def test_tab_tooltips(driver):
         hover = ActionChains(driver).move_to_element(
             driver.find_element_by_id('react-tabs-0'))
         hover.perform()
-        time.sleep(1)
+        time.sleep(0.8)
         assert driver.find_element_by_xpath(
             "//span[contains(text(),'Manage your projects')]"
         ).is_displayed()
@@ -20,7 +20,7 @@ def test_tab_tooltips(driver):
         hover = ActionChains(driver).move_to_element(
             driver.find_element_by_id('react-tabs-2'))
         hover.perform()
-        time.sleep(1)
+        time.sleep(0.8)
         assert driver.find_element_by_xpath(
             "//span[contains(text(),'Upload your time-series data')]"
             ).is_displayed()
@@ -28,7 +28,7 @@ def test_tab_tooltips(driver):
         hover = ActionChains(driver).move_to_element(
             driver.find_element_by_id('react-tabs-4'))
         hover.perform()
-        time.sleep(1)
+        time.sleep(0.8)
         assert driver.find_element_by_xpath(
             "//span[contains(text(),'Generate features from your time-series data')]"
         ).is_displayed()
@@ -44,7 +44,7 @@ def test_file_upload_tooltips(driver):
         header_file = driver.find_element_by_css_selector('[name=headerFile]')
         hover = ActionChains(driver).move_to_element(header_file)
         hover.perform()
-        time.sleep(1)
+        time.sleep(0.8)
         assert driver.find_element_by_xpath(
             "//span[contains(.,'filename,label')]"
         ).is_displayed()
@@ -52,7 +52,7 @@ def test_file_upload_tooltips(driver):
         tar_file = driver.find_element_by_css_selector('[name=tarFile]')
         hover = ActionChains(driver).move_to_element(tar_file)
         hover.perform()
-        time.sleep(1)
+        time.sleep(0.8)
         assert driver.find_element_by_xpath(
             "//span[contains(.,'Each file in tarball should be formatted as follows')]"
         ).is_displayed()
