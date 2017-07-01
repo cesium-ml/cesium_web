4da237431c4464324fd6dded1797d196a9dbfe06
Stefan van der Walt <stefanv@berkeley.edu>
2017-06-30 18:01:38 -0700

    Use latest baselayer and upgrade app factory to new API

--- cesium_app/app_server.py
+++ cesium_app/app_server.py
@@ -7,8 +7,6 @@ import pathlib
 from baselayer.app.config import Config
 from . import models
 from baselayer.app import model_util
-from baselayer.app.app_server import (handlers as baselayer_handlers,
-                                      settings as baselayer_settings)
 from baselayer.app.config import load_config
 
 # This provides `login`, `complete`, and `disconnect` endpoints
@@ -27,25 +25,25 @@ from .handlers import (
 )
 
 
-def make_app(config_files=None, debug=False):
+def make_app(cfg, baselayer_handlers, baselayer_settings, debug=False):
     """Create and return a `tornado.web.Application` object with specified
     handlers and settings.
 
     Parameters
     ----------
-    config_files : list of str
-        Filenames of configuration files, loaded in the order specified.
-        By default, 'config.yaml.example' is used for defaults and 'config.yaml'
-        for further customizations.
+    cfg : Config
+        Loaded configuration.  Can be specified with '--config'
+        (multiple uses allowed).
+    baselayer_handlers : list
+        Tornado handlers needed for baselayer to function.
+    baselayer_settings : cfg
+        Settings needed for baselayer to function.
     debug : bool
         Whether or not to start the app in debug mode.  In debug mode,
         changed source files are immediately reloaded.
 
     """
-    # Cesium settings
-    cfg = load_config(config_files)
-
-    if cfg['cookie_secret'] == 'abc01234':
+    if baselayer_settings['cookie_secret'] == 'abc01234':
         print('!' * 80)
         print('  Your server is insecure. Please update the secret string ')
         print('  in the configuration file!')
@@ -73,11 +71,10 @@ def make_app(config_files=None, debug=False):
     ]
 
     settings = baselayer_settings
-    settings.update({'autoreload': debug})  # Specify additional settings here
+    # settings.update({})  # Specify additional settings here
 
     app = tornado.web.Application(handlers, **settings)
     models.init_db(**cfg['database'])
     model_util.create_tables()
-    app.cfg = cfg
 
     return app
