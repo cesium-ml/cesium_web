a6cd967cece8fb67084d2ff92a01cf68d67981bd
Ari Crellin-Quick <a.crellinquick@gmail.com>
2018-04-03 16:07:26 -0700

    Bump tooltip hover wait time in tests for file upload fields

--- cesium_app/tests/frontend/test_tooltips.py
+++ cesium_app/tests/frontend/test_tooltips.py
@@ -33,24 +33,33 @@ def test_tab_tooltips(driver, project):
     ).is_displayed()
 
 
-def test_file_upload_tooltips(driver, project):
+def test_headerfile_upload_tooltip(driver, project):
     driver.get('/')
     driver.refresh()
     driver.find_element_by_id('react-tabs-2').click()
     driver.find_element_by_partial_link_text('Upload new dataset').click()
+    time.sleep(0.2)
 
-    header_file = driver.find_element_by_css_selector('[name=headerFile]')
-    hover = ActionChains(driver).move_to_element(header_file)
+    header_file = driver.find_element_by_css_selector('[name="headerFile"]')
+    hover = ActionChains(driver).click_and_hold(header_file)
     hover.perform()
-    time.sleep(0.8)
+    time.sleep(1)
     assert driver.find_element_by_xpath(
         "//span[contains(.,'filename,label')]"
     ).is_displayed()
 
-    tar_file = driver.find_element_by_css_selector('[name=tarFile]')
-    hover = ActionChains(driver).move_to_element(tar_file)
+
+def test_tarfile_upload_tooltip(driver, project):
+    driver.get('/')
+    driver.refresh()
+    driver.find_element_by_id('react-tabs-2').click()
+    driver.find_element_by_partial_link_text('Upload new dataset').click()
+    time.sleep(0.2)
+
+    tar_file = driver.find_element_by_css_selector('[name="tarFile"]')
+    hover = ActionChains(driver).click_and_hold(tar_file)
     hover.perform()
-    time.sleep(0.8)
+    time.sleep(1)
     assert driver.find_element_by_xpath(
         "//span[contains(.,'Each file in tarball should be formatted as follows')]"
     ).is_displayed()
