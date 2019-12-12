e5e1ee6eec4dea706aae3d143d18c11a3a4a240e
Ari Crellin-Quick <a.crellinquick@gmail.com>
2019-12-12 14:26:56 -0800

    Use expected conditions in tooltips tests

--- cesium_app/tests/frontend/test_tooltips.py
+++ cesium_app/tests/frontend/test_tooltips.py
@@ -1,6 +1,8 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
@@ -9,29 +11,31 @@ def test_tab_tooltips(driver, project):
     driver.get('/')
     driver.refresh()
 
-    hover = ActionChains(driver).click_and_hold(
-        driver.find_element_by_id('react-tabs-0'))
-    hover.perform()
-    time.sleep(1.5)
-    assert driver.find_element_by_xpath(
-        "//span[contains(.,'Manage your projects')]"
-    ).is_displayed()
+    ActionChains(driver).move_to_element(
+        driver.find_element_by_id('react-tabs-0')).perform()
+    WebDriverWait(driver, 10).until(EC.visibility_of_element_located((
+        By.XPATH, "//span[contains(.,'Manage your projects')]")))
+    # assert driver.find_element_by_xpath(
+    #     "//span[contains(.,'Manage your projects')]"
+    # ).is_displayed()
 
-    hover = ActionChains(driver).click_and_hold(
-        driver.find_element_by_id('react-tabs-2'))
-    hover.perform()
-    time.sleep(1.5)
-    assert driver.find_element_by_xpath(
-        "//span[contains(.,'Upload your time-series data')]"
-        ).is_displayed()
+    ActionChains(driver).move_to_element(
+        driver.find_element_by_id('react-tabs-2')).perform()
+    WebDriverWait(driver, 10).until(EC.visibility_of_element_located((
+        By.XPATH, "//span[contains(.,'Upload your time-series data')]")))
+    # time.sleep(1.5)
+    # assert driver.find_element_by_xpath(
+    #     "//span[contains(.,'Upload your time-series data')]"
+    #     ).is_displayed()
 
-    hover = ActionChains(driver).click_and_hold(
-        driver.find_element_by_id('react-tabs-4'))
-    hover.perform()
-    time.sleep(1.5)
-    assert driver.find_element_by_xpath(
-        "//span[contains(.,'Generate features from your time-series data')]"
-    ).is_displayed()
+    ActionChains(driver).move_to_element(
+        driver.find_element_by_id('react-tabs-4')).perform()
+    WebDriverWait(driver, 10).until(EC.visibility_of_element_located((
+        By.XPATH, "//span[contains(.,'Generate features from your time-series data')]")))
+    # time.sleep(1.5)
+    # assert driver.find_element_by_xpath(
+    #     "//span[contains(.,'Generate features from your time-series data')]"
+    # ).is_displayed()
 
 
 def test_headerfile_upload_tooltip(driver, project):
@@ -42,12 +46,13 @@ def test_headerfile_upload_tooltip(driver, project):
     time.sleep(0.2)
 
     header_file = driver.find_element_by_css_selector('[name="headerFile"]')
-    hover = ActionChains(driver).click_and_hold(header_file)
-    hover.perform()
-    time.sleep(1)
-    assert driver.find_element_by_xpath(
-        "//span[contains(.,'filename,label')]"
-    ).is_displayed()
+    ActionChains(driver).click_and_hold(header_file).perform()
+    WebDriverWait(driver, 10).until(EC.visibility_of_element_located((
+        By.XPATH, "//span[contains(.,'filename,label')]")))
+    # time.sleep(1)
+    # assert driver.find_element_by_xpath(
+    #     "//span[contains(.,'filename,label')]"
+    # ).is_displayed()
 
 
 def DISABLED_test_tarfile_upload_tooltip(driver, project):
